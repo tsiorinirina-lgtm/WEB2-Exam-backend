f34f6f957d5ac861ea45191bb615330a1faf92f6
Bandy_bogosy <maatanarakotomalala@gmail.com>
2026-08-23 22:36:47 +0300

    feat: add question model
    * feat(question):add question interface
    
    * feat(question):add QuestionCreateInput interface
    
    * feat(question):add QuestionUpdateInput interface

---
+++ src/models/Question.ts
@@ -0,0 +1,15 @@
+export interface Question {
+    id: string;
+    statement: string;
+    exam_id: string;
+}
+
+export interface QuestionCreateInput {
+    statement: string;
+    exam_id: string;
+    choices: ChoiceCreateInput[];
+}
+
+export interface QuestionUpdateInput {
+    statement?: string;
+}
