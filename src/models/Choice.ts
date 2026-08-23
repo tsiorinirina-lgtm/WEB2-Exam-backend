e71757e12b4e92f0e1cec617fbfd2f979024a131
Bandy_bogosy <maatanarakotomalala@gmail.com>
2026-08-23 22:44:04 +0300

    feat: add choice model
    * feat(choice): add choice interface
    
    * feat(choice): add ChoiceCreateInput interface
    
    * feat(choice): add ChoiceUpdateInput interface
    
    ---------
    
    Co-authored-by: Mahatana Rakotomalala <hei.mahatana@gmail.com>

---
+++ src/models/Choice.ts
@@ -0,0 +1,17 @@
+export interface Choice {
+    id: string;
+    label: string;
+    is_correct: boolean;
+    question_id: string;
+}
+
+export interface ChoiceCreateInput {
+    label: string;
+    is_correct: boolean;
+    question_id: string;
+}
+
+export interface ChoiceUpdateInput {
+    label?: string;
+    is_correct?: boolean;
+}
