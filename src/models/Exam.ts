2277105ed4e627096fbb76b54fee32bbdd589bde
Bandy_bogosy <maatanarakotomalala@gmail.com>
2026-08-23 22:29:12 +0300

    feat: add exam models
    * feat(exam):add Exam interface
    
    * feat(exam):add ExamCreateInput interface
    
    * feat(exam):add ExamUpdateInput interface

---
+++ src/models/Exam.ts
@@ -0,0 +1,24 @@
+export interface Exam {
+    id: string;
+    title: string;
+    description: string | null;
+    date_hour_start: Date;
+    date_hour_end: Date;
+    course_id: string;
+}
+
+export interface ExamCreateInput {
+    title: string;
+    description?: string;
+    date_hour_start: Date | string;
+    date_hour_end: Date | string;
+    course_id: string;
+}
+
+export interface ExamUpdateInput {
+    title?: string;
+    description?: string;
+    date_hour_start?: Date | string;
+    date_hour_end?: Date | string;
+    course_id?: string;
+}
