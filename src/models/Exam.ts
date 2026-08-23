20327516f2849f67892d7abadc997f0de1f4ca7b
Mahatana Rakotomalala <hei.mahatana@gmail.com>
2026-08-23 22:27:28 +0300

    feat(exam):add ExamUpdateInput interface

--- src/models/Exam.ts
+++ src/models/Exam.ts
@@ -8,9 +8,17 @@ export interface Exam {
 }
 
 export interface ExamCreateInput {
-  title: string;
-  description?: string;
-  date_hour_start: Date | string;
-  date_hour_end: Date | string;
-  course_id: string;
+    title: string;
+    description?: string;
+    date_hour_start: Date | string;
+    date_hour_end: Date | string;
+    course_id: string;
+}
+
+export interface ExamUpdateInput {
+    title?: string;
+    description?: string;
+    date_hour_start?: Date | string;
+    date_hour_end?: Date | string;
+    course_id?: string;
 }
