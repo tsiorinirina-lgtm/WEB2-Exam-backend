5720b9340593f522fa91a7b38358c5d3facd3904
Mahatana Rakotomalala <hei.mahatana@gmail.com>
2026-08-25 23:10:51 +0300

    feat: add MyExamQuestion interface

--- src/models/StudentExam.ts
+++ src/models/StudentExam.ts
@@ -2,3 +2,11 @@ export interface StudentChoice {
     id: number;
     text: string;
 }
+
+export interface MyExamQuestion {
+    id: number;
+    statement: string;
+    points: number;
+    position: number;
+    choices: StudentChoice[];
+}
