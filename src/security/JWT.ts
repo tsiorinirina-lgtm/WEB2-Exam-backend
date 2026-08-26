f996720602222cb074bfab2658046a24c5d3f32f
heitsiorinirina-lgtm <hei.tsiorinirina@gmail.com>
2026-08-26 10:19:03 +0300

    feat: add jwt token verification

---
+++ src/security/JWT.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import type { AuthenticatedUser } from "../models/User.ts";
+
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET || "";
+
+export const verifyAccessToken = (token: string): AuthenticatedUser =>
+  jwt.verify(token, JWT_SECRET) as AuthenticatedUser;
