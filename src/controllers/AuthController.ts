086ad09332dba6f5ff467843997a096646b90ee1
heitsiorinirina-lgtm <hei.tsiorinirina@gmail.com>
2026-08-26 22:21:21 +0300

    fix(auth): correct email field name in login validation and improve error handling

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,8 +1,7 @@
 import type { Request, Response, NextFunction, Express } from "express";
 import { AuthService } from "../services/AuthService.ts";
-import { authenticateUser } from "../security/AuthMiddleware.ts";
 import BadRequest from "../errors/BadRequest.ts";
-import { InternalServerError } from "../errors/InternalServer.ts";
+import { HttpError } from "../errors/HttpError.ts";
 
 export class AuthController {
   private authService: AuthService;
@@ -27,22 +26,22 @@ export class AuthController {
     next: NextFunction,
   ): Promise<void> => {
     try {
-      if (!req.body.mail || !req.body.password) {
+      if (!req.body.email || !req.body.password) {
         const error = new BadRequest(
           "Please check the validity of your request",
         );
         res.status(error.statusCode).json({ message: error.message });
+        return;
       }
       const { email, password } = req.body;
       const authentication = await this.authService.login({ email, password });
       res.status(200).json({ authentication });
     } catch (error) {
-      const errorResponse = new InternalServerError(
-        "We can't process your request now, please try again later",
-      );
-      res
-        .status(errorResponse.statusCode)
-        .json({ message: errorResponse.message });
+      if (error instanceof HttpError) {
+        res.status(error.statusCode).json({ message: error.message });
+        return;
+      }
+      next(error);
     }
   };
 }
