b4dbf2d3abd6c38b95ffe8265cf983b818785435
heitsiorinirina-lgtm <hei.tsiorinirina@gmail.com>
2026-08-26 11:33:17 +0300

    feat: implement user authentication middleware

---
+++ src/security/AuthMiddleware.ts
@@ -0,0 +1,29 @@
+import type { RequestHandler } from "express";
+import { verifyAccessToken } from "./JWT.ts";
+import { UnauthorizedError } from "../errors/Unauthorized.ts";
+import type { AuthenticatedUser } from "../models/User.ts";
+
+const BEARER_PREFIX = "Bearer ";
+
+declare global {
+  namespace Express {
+    interface Request {
+      authUser: AuthenticatedUser;
+    }
+  }
+}
+
+export const authenticateUser: RequestHandler = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith(BEARER_PREFIX)) {
+    const error = new UnauthorizedError("Invalid access token");
+    return res.status(error.statusCode).json({ message: error.message });
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    req.authUser = verifyAccessToken(token);
+    next();
+  } catch (error) {
+    return res.send(error);
+  }
+};
