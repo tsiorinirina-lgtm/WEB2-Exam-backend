fb85726d972cc1b00c036af89b560bcd1a900e1f
heitsiorinirina-lgtm <hei.tsiorinirina@gmail.com>
2026-08-26 17:14:42 +0300

    refactor(auth): remove jwtPayload interface as it is redundant

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,49 +1,50 @@
-export type Role = 'admin' | 'student';
+export type Role = "admin" | "student";
 
 export interface User {
-    id: number;
-    email: string;
-    name: string;
-    password_hash: string;
-    is_active: boolean;
-    joined_at: Date;
-    role: Role;
+  id: number;
+  email: string;
+  name: string;
+  password_hash: string;
+  is_active: boolean;
+  joined_at: Date;
+  role: Role;
 }
 
 export interface UserCreateDTO {
-    email: string;
-    name: string;
-    password_hash: string;
-    role: Role;
+  email: string;
+  name: string;
+  password_hash: string;
+  role: Role;
 }
 
 export interface UserUpdateDTO {
-    email?: string;
-    name?: string;
-    is_active?: boolean;
-    password_hash?: string;
+  email?: string;
+  name?: string;
+  is_active?: boolean;
+  password_hash?: string;
 }
 
 export interface AuthenticatedUser {
-    id: string;
-    mail: string;
-    name: string;
-    is_active: boolean;
-    created_at: Date;
-    role: Role;
+  id: string;
+  mail: string;
+  name: string;
+  is_active: boolean;
+  created_at: Date;
+  role: Role;
 }
 
 export interface UserCredential {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 export interface JwtPayload {
-    userId: number;
-    role: Role;
+  userId: number;
+  role: Role;
 }
 
 export interface LoginResponseDTO {
-    token: string;
-    user: AuthenticatedUser;
+  accessToken: string;
+  refreshToken: string;
+  user: AuthenticatedUser;
 }
