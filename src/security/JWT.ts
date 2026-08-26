9d88ea491833f7b2db092fcbe8df7b6de537cd75
heitsiorinirina-lgtm <hei.tsiorinirina@gmail.com>
2026-08-26 19:49:06 +0300

    refactor(auth): rename token signing and verification functions for both refresh and access tokens

--- src/security/JWT.ts
+++ src/security/JWT.ts
@@ -1,13 +1,13 @@
 import dotenv from "dotenv";
-import jwt from "jsonwebtoken";
+import jwt, { type SignOptions } from "jsonwebtoken";
 import type { AuthenticatedUser } from "../models/User.ts";
 
 dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET || "";
 
-export const verifyAccessToken = (token: string): AuthenticatedUser =>
+export const verifyToken = (token: string): AuthenticatedUser =>
   jwt.verify(token, JWT_SECRET) as AuthenticatedUser;
 
-export const signAccessToken = (user: AuthenticatedUser): string =>
-  jwt.sign(user, JWT_SECRET);
+export const signToken = (user: AuthenticatedUser): string =>
+  jwt.sign(user, JWT_SECRET, { expiresIn: "15Min" } as SignOptions);
