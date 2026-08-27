f79dad2c9713cfa37a166df429b7dc7113b8f3c3
Mahatana Rakotomalala <maatanarakotomalala@gmail.com>
2026-08-27 12:18:11 +0300

    refactor: change controller folder name to controllers

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 import type { Express, Request, Response } from "express";
+import { ExamController } from "./src/controllers/ExamController.ts";
 import express from "express";
 import "dotenv/config";
 const app: Express = express();
 const port = process.env.PORT || 3000;
-
+new ExamController(app);
 app.get("/ping", (req: Request, res: Response) => {
   res.send("pong");
 });
