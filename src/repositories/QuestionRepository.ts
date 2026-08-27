8751278b17dd48a60299fdf7b7c6facb8b4e43cb
Mahatana Rakotomalala <maatanarakotomalala@gmail.com>
2026-08-28 00:07:08 +0300

    feat): initialize QuestionRepository and constructor

---
+++ src/repositories/QuestionRepository.ts
@@ -0,0 +1,11 @@
+import { Pool, QueryResult } from 'pg';
+import type{ Question, QuestionInput } from '../models/Question.js';
+import type { Choice, ChoiceInput } from '../models/Choice.js';
+
+export class QuestionRepository {
+    private pool: Pool;
+
+    constructor(pool: Pool) {
+        this.pool = pool;
+    }
+}
