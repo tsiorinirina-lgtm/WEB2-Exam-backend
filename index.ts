44cfdef201245c8d290f36f834baaa87c6e001c4
heitsiorinirina-lgtm <hei.tsiorinirina@gmail.com>
2026-08-27 12:10:00 +0300

    feat: initialize question controller in index.ts

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import type { Express, Request, Response } from "express";
 import type { AuthenticatedUser } from "./src/models/User.ts";
+import { QuestionController } from "./src/controllers/QuestionController.ts";
 import express from "express";
 import "dotenv/config";
 import { AuthController } from "./src/controllers/AuthController.ts";
 const app: Express = express();
 app.use(express.json());
 new AuthController(app);
+new QuestionController(app);
 const port = process.env.PORT || 3000;
 
 app.get("/ping", (req: Request, res: Response) => {
