eead5a3347cb4d3c0f0e4755b942a397c72ec532
heitsiorinirina-lgtm <hei.tsiorinirina@gmail.com>
2026-08-28 12:15:36 +0300

    feat: add findById method in userRepository

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -8,7 +8,20 @@ export class UserRepository {
     this.pool = pool;
   }
 
-  getAll;
+  findById = async (id: number): Promise<User> => {
+    const client = await this.pool.connect();
+    try {
+      const user = await client.query("SELECT * FROM users WHERE id = $1", [
+        id,
+      ]);
+      return user.rows[0];
+    } catch (error) {
+      throw error;
+    } finally {
+      client.release();
+    }
+  };
+
   findByMail = async (email: string): Promise<User> => {
     const client = await this.pool.connect();
     try {
