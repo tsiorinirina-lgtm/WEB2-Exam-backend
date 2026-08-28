c5f45561c81fb53bc3699bd9ce2aef94fa759b39
heitsiorinirina-lgtm <hei.tsiorinirina@gmail.com>
2026-08-28 16:25:02 +0300

    feat: list courses through service

--- src/services/CourseService.ts
+++ src/services/CourseService.ts
@@ -1,10 +1,15 @@
 import { Pool } from "pg";
 import { CourseRepository } from "../repositories/CourseRepository.ts";
+import type { Course } from "../models/Course.ts";
 
 export class CourseService {
   private courseRepository: CourseRepository;
 
   constructor(pool = new Pool()) {
     this.courseRepository = new CourseRepository(pool);
+  }
+
+  async getAllCourses(): Promise<Course[]> {
+    return this.courseRepository.getAll();
   }
 }
