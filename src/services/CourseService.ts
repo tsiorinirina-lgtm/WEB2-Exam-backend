64a168755cef7a47e4e6ea6453a945480cba9a8e
heitsiorinirina-lgtm <hei.tsiorinirina@gmail.com>
2026-08-28 16:28:00 +0300

    feat: create courses through service

--- src/services/CourseService.ts
+++ src/services/CourseService.ts
@@ -1,6 +1,6 @@
 import { Pool } from "pg";
 import { CourseRepository } from "../repositories/CourseRepository.ts";
-import type { Course } from "../models/Course.ts";
+import type { Course, CourseCreateDTO } from "../models/Course.ts";
 
 export class CourseService {
   private courseRepository: CourseRepository;
@@ -11,5 +11,9 @@ export class CourseService {
 
   async getAllCourses(): Promise<Course[]> {
     return this.courseRepository.getAll();
+  }
+
+  async createCourse(courseData: CourseCreateDTO): Promise<Course> {
+    return this.courseRepository.create(courseData);
   }
 }
