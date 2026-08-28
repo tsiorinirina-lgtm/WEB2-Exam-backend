05d48b969d8acc4aa32e4b44ac7db2806971b84d
heitsiorinirina-lgtm <hei.tsiorinirina@gmail.com>
2026-08-28 11:12:53 +0300

    feat: add getAll method in student service class

--- src/services/StudentService.ts
+++ src/services/StudentService.ts
@@ -1,10 +1,19 @@
 import { StudentRepository } from "../repositories/StudentRepository.ts";
 import { pool } from "../config/database.ts";
+import type { Student } from "../models/Student.ts";
 
 export class StudentService {
   private studentRepository: StudentRepository;
 
-  constructor(studentRepository: StudentRepository) {
+  constructor() {
     this.studentRepository = new StudentRepository(pool);
   }
+
+  getAll = async (): Promise<Student[]> => {
+    try {
+      return await this.studentRepository.getAllStudents();
+    } catch (error) {
+      throw error;
+    }
+  };
 }
