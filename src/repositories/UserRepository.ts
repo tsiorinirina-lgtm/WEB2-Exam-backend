4045df41711346c6a99c1c64371c88dee30616fe
heitsiorinirina-lgtm <hei.tsiorinirina@gmail.com>
2026-08-28 11:54:41 +0300

    feat: add find by Email method in user repository

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -7,4 +7,19 @@ export class UserRepository {
   constructor(pool: Pool) {
     this.pool = pool;
   }
+
+  getAll;
+  findByMail = async (email: string): Promise<User> => {
+    const client = await this.pool.connect();
+    try {
+      const user = await client.query("SELECT * FROM users WHERE mail = $1", [
+        email,
+      ]);
+      return user.rows[0];
+    } catch (error) {
+      throw error;
+    } finally {
+      client.release();
+    }
+  };
 }
