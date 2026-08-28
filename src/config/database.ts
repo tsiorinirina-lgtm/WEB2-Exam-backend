727a335d69332c580681a76ef8c7e9f88f27e2c9
heitsiorinirina-lgtm <hei.tsiorinirina@gmail.com>
2026-08-28 13:18:22 +0300

    refactor: remove max connection limit from database pool configuration

--- src/config/database.ts
+++ src/config/database.ts
@@ -21,7 +21,6 @@ export const pool = new Pool({
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  max: 10,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 5000,
 });
