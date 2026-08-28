eb674653cb6cea408967556620408a47117344d9
heitsiorinirina-lgtm <hei.tsiorinirina@gmail.com>
2026-08-28 17:10:00 +0300

    feat: list exams through service

--- src/services/ExamService.ts
+++ src/services/ExamService.ts
@@ -1,10 +1,15 @@
 import { ExamRepository } from "../repositories/ExamRepository.ts";
 import { pool } from "../config/database.ts";
+import type { Exam } from "../models/Exam.ts";
 
 export class ExamService {
   private examRepository: ExamRepository;
 
   constructor() {
     this.examRepository = new ExamRepository(pool);
+  }
+
+  async getAll(): Promise<Exam[]> {
+    return this.examRepository.findAll();
   }
 }
